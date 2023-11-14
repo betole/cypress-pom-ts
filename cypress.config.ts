a98d166f8339d690cd19e3b613035e1b7470dbeb
betole <c.mcbottle@gmail.com>
2023-11-13 23:41:48 -0300

    Env loading from fixture

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,29 @@
+/// <reference types="cypress"/>
+
 import { defineConfig } from "cypress";
+import { resolve } from 'node:path';
+import { readFileSync } from 'node:fs';
 
 export default defineConfig({
+  retries: {
+    runMode: 1
+  },
+  chromeWebSecurity: false,
+  numTestsKeptInMemory: 5,
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  video: false,
+  includeShadowDom: true,
+  screenshotsFolder: './reports/screenshots',
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      const env = config.env.ENV?? 'local';
+      const envFilePath = resolve(`cypress/fixtures/envs/${env}`, 'config.json');
+      const fixtureConfig = JSON.parse(readFileSync(envFilePath, 'utf-8'));
+      return {
+        ...config,
+        ...fixtureConfig
+      }
     },
   },
 });
